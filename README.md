eedf79d513664f1e704a81f52674f9a8daed2166
tlatta13 <tlatta1313@gmail.com>
2020-06-02 19:46:50 -0400

    Reworked server.js and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# note-taker
+# Note Taker Application
+## Description
+This project required us to build an application that allows users to create notes and reminders for themselves. Once the user has created their note they able to save them to a list and also delete old notes.  
+## Table of Content
+
+1. [Photos](#photos)
+2. [Site Links](#siteLinks)
+
+<a name="accomplishments"></a>
+## Key Accomplishments
+* Created a server to run the application. 
+* Used express static to properly link the stylesheet
+* 
+
+<a name="photos"></a>
+## Photos / Videos
+* [Application Demo Video](https://vimeo.com/424399672)
+* ![Team Page](/Assets/Team-Builder-Demo.png)
+
+<a name="siteLinks"></a>
+## Site Links
+* [Repository](https://github.com/tlatta13/team-profiles.git)
+* [Deployed Application](https://tlatta13.github.io/team-profiles/)
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const notes = JSON.parse(fs.readFileSync(__dirname + "/db/db.json", "utf-8", (err) => {
+    if(err) throw err;
+})) || {};
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -17,42 +20,29 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 })
 
-app.get("/api/notes", function(req, res) {
-    let json = pullJSON();
-    res.json(json);
-    console.log(json)
+app.get("/api/notes", function (req, res) {
+    const note = fs.readFileSync(__dirname + "/db/db.json");
+    return res.json(note);
 });
 
 app.post("/api/notes", function(req, res) {
-});
+    let newNote = req.body;
+    newNote.id = notes.length + 1;
+    console.log(newNote);
+    notes.push(newNote); 
 
-app.delete("/api/notes/:id", function(req, res) {
+    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if(err) throw err;
+    })
 
-});
+    return res.json(notes);
+})
+
+// app.delete("/api/notes/:id", function(req, res) {
+//     let removeNote = req.params.id;
+//     return res.json();
+// });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 })
-
-
-function pullJSON() {
-    let note = fs.readFileSync(__dirname + "/db/db.json");
-    let json = JSON.parse(note);
-    return json;
-}
-
-function newNoteObj(data) {
-
-}
-
-function addNote(data) {
-
-}
-
-function saveNote() {
-
-}
-
-function deleteNote() {
-    
-}
