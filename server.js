6be699c90acb49b15c7cdbf412bcffd5d0722824
tlatta13 <tlatta1313@gmail.com>
2020-06-03 16:47:50 -0400

    got the save and delete functionality working

--- server.js
+++ server.js
@@ -8,20 +8,17 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const notes = JSON.parse(fs.readFileSync(__dirname + "/db/db.json", "utf-8", (err) => {
+let notes = JSON.parse(fs.readFileSync(__dirname + "/db/db.json", (err) => {
     if(err) throw err;
-})) || {};
+}));
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-})
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
 app.get("/api/notes", function (req, res) {
-    const note = fs.readFileSync(__dirname + "/db/db.json");
+    // const note = require("./db/db.json");
+    const note = JSON.parse(fs.readFileSync(__dirname + "/db/db.json"));
     return res.json(note);
 });
 
@@ -30,19 +27,33 @@ app.post("/api/notes", function(req, res) {
     newNote.id = notes.length + 1;
     console.log(newNote);
     notes.push(newNote); 
+    
+    fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if(err) throw err;
+    });
+
+    res.json(true);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    const id = parseInt(req.params.id);
+    let newID = 0
+
+    notes = notes.filter(function(note) {
+        return note.id !== id;
+    })
+
+    for (note of notes) {
+        note.id = newID;
+        newID++;
+    }
+    res.json(true)
 
     fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes, null, 2), (err) => {
         if(err) throw err;
     })
-
-    return res.json(notes);
-})
-
-// app.delete("/api/notes/:id", function(req, res) {
-//     let removeNote = req.params.id;
-//     return res.json();
-// });
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-})
+});
